c2391f026bbff6573559c1f68247eecb0ba1cafa
Laserlicht <13953785+Laserlicht@users.noreply.github.com>
2024-11-30 21:21:34 +0100

    fix

--- helper.py
+++ helper.py
@@ -3,11 +3,11 @@ from PIL import Image
 
 def get_stroke(origin_image, border):
     img = np.asarray(origin_image)
+    img_alpha = img[:, :, 3]
     img_new = np.asarray(Image.new(origin_image.mode, (origin_image.width, origin_image.height), (255, 255, 255, 0))).copy()
-    width, height = origin_image.size
-    for x in range(width):
-        for y in range(height):
-            area = img[:, :, 3][x-border:x+border, y-border:y+border]
+    for x in range(img_alpha.shape[0]):
+        for y in range(img_alpha.shape[1]):
+            area = img_alpha[x-border:x+border, y-border:y+border]
             if area.shape[0] == 0 or area.shape[1] == 0:
                 continue
 
