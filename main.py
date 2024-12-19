34573246b65b7c091655392f14f96ad9d7bb4720
George King <98261225+GeorgeK1ng@users.noreply.github.com>
2024-12-19 10:12:13 +0100

    Update main.py
    Added messagebox header

--- main.py
+++ main.py
@@ -41,18 +41,18 @@ def main():
         extract_assets(input_path, temp_path)
         create_mod(temp_path, output_path)
     else:
-        messagebox.showinfo("showinfo", "Please select the installation folder of Heroes of Might & Magic III - HD Edition.")
+        messagebox.showinfo("Heroes of Might & Magic III - HD Edition for VCMI creator", "Please select the installation folder of Heroes of Might & Magic III - HD Edition.")
         input_path = askdirectory()
         if not isinstance(input_path, str) or not os.path.exists(os.path.join(input_path, "HOMM3 2.0.exe")):
-            messagebox.showinfo("showerror", "Heroes of Might & Magic III - HD Edition folder not found!")
+            messagebox.showinfo("Heroes of Might & Magic III - HD Edition for VCMI creator", "Heroes of Might & Magic III - HD Edition folder not found!")
             return
-        messagebox.showinfo("showinfo", "Please select the output folder for the created mod.") 
+        messagebox.showinfo("Heroes of Might & Magic III - HD Edition for VCMI creator", "Please select the output folder for the created mod.") 
         output_path = askdirectory()
         if not isinstance(output_path, str):
-            messagebox.showinfo("showerror", "No output folder selected!")
+            messagebox.showinfo("Heroes of Might & Magic III - HD Edition for VCMI creator", "No output folder selected!")
             return
         
-        messagebox.showinfo("showinfo", "The conversion process will start after pressing OK. It will run in the background and may take some time (~1 hour). Please be patient. You will be notified upon completion.") 
+        messagebox.showinfo("Heroes of Might & Magic III - HD Edition for VCMI creator", "The conversion process will start after pressing OK. It will run in the background and may take some time (~1 hour). Please be patient. You will be notified upon completion.") 
         
         temp_path = tempfile.TemporaryDirectory()
         extract_assets(input_path, temp_path.name)
@@ -62,7 +62,7 @@ def main():
         except:
             pass
 
-        messagebox.showinfo("showinfo", "The conversion process is complete. Please copy the folder in the output directory to the mod directory of VCMI.")         
+        messagebox.showinfo("Heroes of Might & Magic III - HD Edition for VCMI creator", "The conversion process is complete. Please copy the folder in the output directory to the mod directory of VCMI.")         
 
 if __name__ == '__main__':
     multiprocessing.freeze_support()
