ee7edab63164717d2bdb4a1c95aff5e05c7a049e
Laserlicht <13953785+Laserlicht@users.noreply.github.com>
2024-12-20 01:19:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -51,12 +51,14 @@ class Progress(Tk):
         self.progress.pack(pady=10, padx=10)
 
         def update(destpaths):
+            try:
             while True:
                 max_size = 4_793_170_141 + 3_698_241_492
                 size = sum([sum(f.stat().st_size for f in Path(destpath).glob('**/*') if f.is_file()) for destpath in destpaths])
                 percent = size / max_size
                 self.progress.step(percent)
                 time.sleep(5)
+            except: pass
         threading.Thread(target=update, args=(destpaths,)).start()
 
 def progresswindow(destpaths):
